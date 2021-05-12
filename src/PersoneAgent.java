4b6bbb043e5ebf633cc5a4e1c2ecce91fb738573
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-05-12 18:12:32 +0000

    agent leave restaurant behaviour

--- src/PersoneAgent.java
+++ src/PersoneAgent.java
@@ -1,5 +1,6 @@
 import jade.core.Agent;
 
+import java.io.IOException;
 import java.util.Random;
 
 import jade.core.AID;
@@ -14,24 +15,24 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 public class PersoneAgent extends Agent {
 
 	private boolean status;
-
 	private AID[] restaurants;
-	
+	private long time;
 	private int indexRandomRestaurant;
 
 	protected void setup() {
-		
 		System.out.println("Persone : "+getAID().getLocalName()+" is ready.");
-		Random rand = new Random(); 
-		addBehaviour(new TickerBehaviour(this, rand.nextInt(30000) + 10000) {
+		Random rand = new Random();
+		time = rand.nextInt(30000) + 10000;
+		
+		addBehaviour(new TickerBehaviour(this, time) {
 			protected void onTick() {
-				
 				System.out.println("\nPersone : "+getAID().getLocalName()+" Trying to reserve a place.");
 
 				DFAgentDescription template = new DFAgentDescription();
 				ServiceDescription sd = new ServiceDescription();
 				sd.setType("reservation-restaurant");
 				template.addServices(sd);
+				
 				try {
 					DFAgentDescription[] result = DFService.search(myAgent, template); 
 					System.out.println("Persone : "+getAID().getLocalName()+" found the following restaurant :");
@@ -40,17 +41,17 @@ public class PersoneAgent extends Agent {
 					for (int i = 0; i < result.length; ++i) {
 						restaurants[i] = result[i].getName();
 						System.out.println("\t" + restaurants[i].getLocalName());
-						}
+					}
 				}
 				catch (FIPAException fe) {
 					fe.printStackTrace();
 				}
-
+			
 				myAgent.addBehaviour(new CallForReservation());
 
 			}
 		} );
-}
+	}
 
 	protected void takeDown() {
 		System.out.println("Persone : "+getAID().getLocalName()+" terminating.\n");
@@ -72,7 +73,14 @@ public class PersoneAgent extends Agent {
 				indexRandomRestaurant = rand.nextInt(restaurants.length);
 				System.out.println("Personne : " + getAID().getLocalName() + " chose the restaurant : " + restaurants[indexRandomRestaurant].getLocalName());
 				cfp.addReceiver(restaurants[indexRandomRestaurant]);
-				cfp.setContent("1");
+				
+				try {
+					cfp.setContentObject(new Message("reserver-restaurant","1"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 				cfp.setReplyWith("cfp"+System.currentTimeMillis());
 				myAgent.send(cfp);
 
@@ -86,7 +94,12 @@ public class PersoneAgent extends Agent {
 				if (reply != null) {
 					if (reply.getPerformative() == ACLMessage.INFORM) {
 						System.out.println("Persone : "+getAID().getLocalName()+" successfully reserved a place from "+reply.getSender().getLocalName());
-						myAgent.doDelete();
+						
+						addBehaviour(new WakerBehaviour(myAgent, time/2) {
+							protected void handleElapsedTimeout() {
+								addBehaviour(new LeaveRestaurant());
+							}
+						});
 					}
 					else {
 						System.out.println("Persone : "+getAID().getLocalName()+" failed to reserve from "+reply.getSender().getLocalName());
@@ -107,4 +120,58 @@ public class PersoneAgent extends Agent {
 		}
 	}
 	
+	private class LeaveRestaurant extends Behaviour {
+
+		private MessageTemplate mt;
+		private int step = 0;
+
+		public void action() {
+			
+			switch (step) {
+			case 0:
+
+				System.out.println( "\nPerson : " + myAgent.getLocalName() + " is going to leave restaurant " + restaurants[indexRandomRestaurant].getLocalName());
+				
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				cfp.addReceiver(restaurants[indexRandomRestaurant]);
+				
+				try {
+					cfp.setContentObject(new Message("leave-restaurant","1"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				cfp.setReplyWith("cfp"+System.currentTimeMillis());
+				myAgent.send(cfp);
+
+				mt = MessageTemplate.MatchInReplyTo(cfp.getReplyWith());
+				step = 1;
+
+				break;
+			case 1:
+				ACLMessage reply = myAgent.receive(mt);
+				if (reply != null) {
+					if (reply.getPerformative() == ACLMessage.INFORM) {
+						System.out.println("Persone : "+getAID().getLocalName()+" finished all tasks "+reply.getSender().getLocalName());
+						myAgent.doDelete();
+					}
+					else {
+						System.out.println("Persone : "+getAID().getLocalName()+" failed to leave from "+reply.getSender().getLocalName());
+					}
+					step = 2;
+				}
+				else {
+					block();
+				}
+
+				break;
+			}        
+		}
+
+		public boolean done() {
+			return (step == 2 );
+		}
+	}
+
 }
--- src/RestaurantAgent.java
+++ src/RestaurantAgent.java
@@ -8,13 +8,13 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 
 
 public class RestaurantAgent extends Agent {
 
-	private int C;
-
-	private int Ct;
+	private int capacity;
+	private int availablePlaces;
 	
 	protected void setup() {
 
@@ -22,9 +22,9 @@ public class RestaurantAgent extends Agent {
 		
 		if (args != null && args.length > 0) {
 
-			C = Integer.parseInt(args[0].toString());
+			capacity = Integer.parseInt(args[0].toString());
 			
-			Ct=C;
+			availablePlaces=capacity;
 	
 			DFAgentDescription dfd = new DFAgentDescription();
 			dfd.setName(getAID());
@@ -41,7 +41,7 @@ public class RestaurantAgent extends Agent {
 	
 			addBehaviour(new ReciveCallService());
 	
-			System.out.println("Restaurant : "+getAID().getLocalName()+" with capacity "+C+" is ready.");
+			System.out.println("Restaurant : "+getAID().getLocalName()+" with capacity "+capacity+" is ready.");
 
 		}
 		else {
@@ -73,28 +73,44 @@ public class RestaurantAgent extends Agent {
 			ACLMessage msg = myAgent.receive(mt);
 			
 			if (msg != null) {
-
-				int requestPlaces =Integer.parseInt(msg.getContent());
-				ACLMessage reply = msg.createReply();
-				
-				System.out.println("Restaurant : " + myAgent.getLocalName() + " recieved a call from : " + msg.getSender().getLocalName());
-
-				if (requestPlaces <= Ct) {
-					Ct-=requestPlaces;
-					reply.setPerformative(ACLMessage.INFORM);
-					System.out.println("Restaurant : "+msg.getSender().getLocalName()+" reserved "+requestPlaces+" places in "+myAgent.getLocalName());
-					System.out.println("Restaurant :"+myAgent.getLocalName()+" has "+Ct+" places disponible ");
-					if(Ct==0) {
-						System.out.println("Restaurant : "+myAgent.getLocalName()+" is fully occupied.");
-						//myAgent.doDelete();
-					}
+				try {
+					Message messageContent = (Message) msg.getContentObject();
+					
+					if(messageContent.getType().equals("reserver-restaurant")) {
+						int requestPlaces = Integer.parseInt(messageContent.getContent());
+						ACLMessage reply = msg.createReply();
 						
+						System.out.println("Restaurant : " + myAgent.getLocalName() + " recieved a call from : " + msg.getSender().getLocalName());
+
+						if (requestPlaces <= availablePlaces) {
+							availablePlaces-=requestPlaces;
+							reply.setPerformative(ACLMessage.INFORM);
+							System.out.println("Restaurant : "+msg.getSender().getLocalName()+" reserved "+requestPlaces+" places in "+myAgent.getLocalName() + " (" + capacity +" places availables)");
+							if(availablePlaces==0) {
+								System.out.println("Restaurant : "+myAgent.getLocalName()+" is fully occupied.");
+								//myAgent.doDelete();
+							}
+								
+						}
+						else {
+							reply.setPerformative(ACLMessage.FAILURE);
+							System.out.println("Restaurant : "+myAgent.getLocalName()+" no places for "+msg.getSender().getLocalName());
+						}
+						myAgent.send(reply);
+					}else if (messageContent.getType().equals("leave-restaurant")) {
+						int nbPlaces = Integer.parseInt(messageContent.getContent());
+						
+						availablePlaces = (availablePlaces + nbPlaces) <= capacity ? (availablePlaces + nbPlaces) : capacity;
+						
+						ACLMessage reply = msg.createReply();
+						reply.setPerformative(ACLMessage.INFORM);
+						System.out.println("Restaurant : "+msg.getSender().getLocalName()+" left restaurant : "+myAgent.getLocalName() + " (" + capacity +" places availables)");
+						myAgent.send(reply);
+					}
+				} catch (UnreadableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				else {
-					reply.setPerformative(ACLMessage.FAILURE);
-					System.out.println("Restaurant : "+myAgent.getLocalName()+" no places for "+msg.getSender().getLocalName());
-				}
-				myAgent.send(reply);
 			}
 			else {
 				block();
